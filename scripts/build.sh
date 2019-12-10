7380191228c16c4da7fe8ea1b0f8b52eb2fa412b
Vinh Nguyen <nhquocvinh@gmail.com>
2019-12-10 23:11:11 +0700

    build prod linux for ec2

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ output="$outputPath/$app"
 src="$srcPath/$app/$pkgFile"
 
 printf "\nBuilding: $app\n"
-time go build -o $output $src
+time env GOOS=linux GOARCH=amd64 go build -o $output $src
 printf "\nBuilt: $app size:"
 ls -lah $output | awk '{print $5}'
 printf "\nDone building: $app\n\n"
